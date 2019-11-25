38ecd586ba9969246dff1df2d889bd4c038f95cf
guga <gustavo.manca@fatec.sp.gov.br>
2019-11-25 02:31:41 -0200

    Iniciado projeto, criada conexao como firebase

---
+++ app/src/main/java/app/manca/carteirinhapet/MainActivity.java
@@ -0,0 +1,14 @@
+package app.manca.carteirinhapet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_create);
+    }
+}
